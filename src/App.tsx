4a69ac02a16563dd0557abdf2b8eb56c4f33a803
Joshua Crosman <JACrosman@gmail.com>
2024-04-03 14:21:47 -0400

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,24 @@ import "./App.css";
 
 import { PhaserGame } from "./phaser/game";
 import { GameRef } from "./phaser/gameRef";
-import { Intermission } from "./phaser/scenes/intermission";
+import {
+  Intermission,
+  IntermissionResults,
+} from "./phaser/scenes/intermission";
+
+import orbyRed from "./assets/orby-sm.png";
+import orbyPurple from "./assets/orby-sm-shadow.png";
 
 function App() {
   const [gameContainerMounted, setGameContainerMounted] = useState(false);
   const [showLabels, setShowLabels] = useState(true);
+  const [selection, setSelection] = useState({
+    text: "",
+    icon: "",
+    isRed: false,
+  });
+  const [isRunning, setIsRunning] = useState(false);
+  const [results, setResults] = useState<IntermissionResults | null>(null);
 
   useEffect(() => {
     // Set game container mounted to true when component mounts
@@ -22,30 +35,118 @@ function App() {
   function startGame() {
     // Access the Phaser game object from the ref and call the start method
     if (GameRef.game) {
-      (GameRef.game.scene.scenes[0] as Intermission).generate({
-        speed: 75,
+      const selection: string = (
+        GameRef.game.scene.scenes[0] as Intermission
+      ).generate({
+        speed: 40,
         minOrbs: 10,
         maxOrbs: 15,
         showLabels,
+        onSelection: (results) => {
+          setIsRunning(false);
+          setResults(results);
+        },
+        time: 4000,
       }); // Assuming the start method is defined in the first scene
+      const isRed = selection.includes("O");
+      setSelection({
+        text: selection.replace(" O", "").replace(" P", ""),
+        icon: isRed ? orbyRed : orbyPurple,
+        isRed,
+      });
+      setIsRunning(true);
+      setResults(null);
+    }
+  }
+
+  const selectionColor = selection?.isRed
+    ? "text-orange-500"
+    : "text-purple-500";
+
+  let resultText = "";
+  let resultColor = "";
+  let resultOrbIcon = "";
+  let resultOrbColor = "";
+
+  if (results) {
+    resultOrbIcon = results.selected?.includes('O') ? orbyRed : orbyPurple;
+    resultOrbColor = results.selected?.includes('O') ? 'text-orange-500' : 'text-purple-500';
+
+    if (results.outOfTime) {
+      resultText = "Out of Time!";
+      resultColor = "text-red-500";
+    } else if (results.win) {
+      resultText = "Correct!";
+      resultColor = "text-green-500";
+    } else {
+      resultText = "Wrong :(";
+      resultColor = "text-red-500";
     }
   }
 
   return (
     <div className="flex flex-col h-screen">
       <div className="p-3 bg-black flex justify-center gap-3">
-        <button
-          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-          onClick={startGame}
-        >
-          Start
-        </button>
-
         <div className="flex items-center gap-2">
-          <input type="checkbox" id="showLabels" checked={showLabels} onChange={e => setShowLabels(e.target.checked)} />
+          <input
+            type="checkbox"
+            id="showLabels"
+            checked={showLabels}
+            onChange={(e) => setShowLabels(e.target.checked)}
+          />
           <label htmlFor="showLabels">Show Labels</label>
         </div>
       </div>
+      {!!selection?.text && (
+        <div
+          className={
+            "flex gap-2 absolute top-1/3 -translate-y-1/3 left-1/2 -translate-x-1/2 text-4xl font-bold bg-[#00000080] px-5 py-2 " +
+            selectionColor
+          }
+        >
+          <div>{selection.text}</div>
+          <img src={selection.icon} width="42px" />
+        </div>
+      )}
+      {!isRunning && !results && (
+        <div className="fixed top-[48px] left-0 right-0 bottom-0 bg-[#00000080]">
+          <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-center flex flex-col gap-4">
+            <button
+              className="bg-black hover:bg-[#0e0e0e] text-white font-semibold py-3 px-4 text-4xl border border-gray-800 rounded shadow"
+              onClick={startGame}
+            >
+              Start
+            </button>
+          </div>
+        </div>
+      )}
+
+      {!!results && (
+        <div className="fixed top-[48px] left-0 right-0 bottom-0 bg-[#00000080]">
+          <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-center flex flex-col gap-4">
+            <div className={`text-8xl ${resultColor} bg-[#00000090] p-6 flex flex-col gap-4`}>
+              {resultText}
+
+              {!results.win && !!results.selected && (
+                <div className="flex flex-col text-3xl justify-center">
+                  <div>You selected</div>
+                  <div className="flex gap-2 justify-center">
+                    <div className={resultOrbColor}>{results.selected?.replace(' O', "").replace(' P', "")}</div>
+                    <img src={resultOrbIcon} width="42px" />
+                  </div>
+                </div>
+              )}
+            </div>
+            <button
+              className="bg-black hover:bg-[#0e0e0e] text-white font-semibold py-3 px-4 text-3xl border border-gray-800 rounded shadow"
+              onClick={startGame}
+            >
+              Try Again
+            </button>
+          </div>
+        </div>
+      )}
+
       {gameContainerMounted && (
         <div id="game-container" className="flex-grow">
           <PhaserGame />
--- src/phaser/scenes/intermission.ts
+++ src/phaser/scenes/intermission.ts
@@ -9,6 +9,14 @@ export interface IntermissionConfig {
   showLabels: boolean;
   minOrbs: number;
   maxOrbs: number;
+  time: number;
+  onSelection: (results: IntermissionResults) => void;
+}
+
+export interface IntermissionResults {
+  win: boolean;
+  outOfTime?: boolean;
+  selected?: string;
 }
 
 export class Intermission extends Phaser.Scene {
@@ -16,6 +24,12 @@ export class Intermission extends Phaser.Scene {
   public orbyGroup: Phaser.Physics.Arcade.Group;
   public orbies: any[] = [];
   public config: IntermissionConfig;
+  public selection: any;
+
+  public timerText: Phaser.GameObjects.Text;
+  public timedEvent: Phaser.Time.TimerEvent;
+  public currentTime: number = 0;
+  public isRunning: boolean = false;
 
   preload() {
     this.load.spritesheet("orbies", orbies, {
@@ -26,7 +40,20 @@ export class Intermission extends Phaser.Scene {
     this.load.image("background", bg);
   }
 
-  update() {
+  update(time, delta) {
+    if (this.isRunning) {
+      this.currentTime -= delta;
+
+      if (this.currentTime <= 0) {
+        this.currentTime = 0;
+        this.pause();
+        this.config.onSelection(false);
+        this.config.onSelection({ win: false, outOfTime: true });
+      }
+
+      this.timerText.setText("Time: " + this.formatTime(this.currentTime)); // Update the text
+    }
+
     for (const orby of this.orbies) {
       orby.getData("text")?.setPosition(orby.x, orby.y);
     }
@@ -39,6 +66,13 @@ export class Intermission extends Phaser.Scene {
       this.game.config.height / backgroundImage.height
     );
 
+    // Create a text object to display the countdown timer
+    this.timerText = this.add.text(200, -350, "Time: " + this.formatTime(this.currentTime), {
+      fontSize: "32px",
+      color: "#ffffff",
+    });
+    this.timerText.setOrigin(0, 0);
+
     this.cameras.main.centerOn(0, 0);
     this.fyrakk = this.physics.add.staticImage(0, 0, "fyrakk").setCircle(50);
   }
@@ -46,7 +80,11 @@ export class Intermission extends Phaser.Scene {
   generate(config: IntermissionConfig) {
     this.config = config;
     this.orbies = this.generateOrbies();
-    // this.start();
+    this.currentTime = this.config.time;
+    this.isRunning = true;
+    this.start();
+
+    return this.selection;
   }
 
   generateOrbies() {
@@ -99,23 +137,31 @@ export class Intermission extends Phaser.Scene {
     const middleStart = numBalls % 2 === 0 ? centerCount - 1 : centerCount;
     const middleEnd = numBalls % 2 === 0 ? middleStart + 2 : middleStart + 1;
 
+    let leftSpeedModifier = 2;
     for (let i = 0; i < middleStart; i++) {
       ballRefs.push(
-        this.generateOrby(getRandomBallColor(), numBalls, i, colorCounts)
+        this.generateOrby(getRandomBallColor(), numBalls, i, colorCounts, false, leftSpeedModifier)
       );
+      leftSpeedModifier -= 0.15;
     }
 
     for (let i = middleStart; i < middleEnd; i++) {
       ballRefs.push(
-        this.generateOrby(getRandomBallColor(), numBalls, i, colorCounts, true)
+        this.generateOrby(getRandomBallColor(), numBalls, i, colorCounts, true, 1.25)
       );
     }
 
+    let rightSpeedModifier = 2;
     for (let i = numBalls - 1; i >= middleEnd; i--) {
       ballRefs.push(
-        this.generateOrby(getRandomBallColor(), numBalls, i, colorCounts)
+        this.generateOrby(getRandomBallColor(), numBalls, i, colorCounts, false, rightSpeedModifier)
       );
+      rightSpeedModifier -= 0.15;
     }
+
+    const randomOrb = Phaser.Utils.Array.GetRandom(ballRefs);
+
+    this.selection = randomOrb.getData("value");
 
     return ballRefs;
   }
@@ -125,7 +171,8 @@ export class Intermission extends Phaser.Scene {
     numBalls: number,
     index: number,
     colorCounts: any,
-    isMiddle = false
+    isMiddle = false,
+    speedModifier: number = 1
   ) {
     const centerCount = Math.floor(numBalls / 2);
     // Define the center position of the semicircle
@@ -150,46 +197,59 @@ export class Intermission extends Phaser.Scene {
     ball.setFrame(ballColor === "red" ? 0 : 1); // Set frame based on color
     ball.setScale(0.75);
     ball.setInteractive();
+    ball.setData('speed', speedModifier);
 
-    if (this.config.showLabels) {
-      let text = "";
+    let text = "";
 
-      if (isMiddle) {
-        if (ballColor === "red") {
-          text = `MID O`;
-        } else {
-          text = `MID P`;
-        }
-      } else if (index < centerCount) {
-        if (ballColor === "red") {
-          text = `L${++colorCounts.leftRedCount} O`;
-        } else {
-          text = `L${++colorCounts.leftPurpleCount} P`;
-        }
+    if (isMiddle) {
+      if (ballColor === "red") {
+        text = `MID O`;
       } else {
-        if (ballColor === "red") {
-          text = `R${++colorCounts.rightRedCount} O`;
-        } else {
-          text = `R${++colorCounts.rightPurpleCount} P`;
-        }
+        text = `MID P`;
       }
-
-      const textObj = this.add.text(ballX, ballY, text, { fontSize: "16px" });
-      textObj.setOrigin(0.5);
-      ball.setData("text", textObj);
+    } else if (index < centerCount) {
+      if (ballColor === "red") {
+        text = `L${++colorCounts.leftRedCount} O`;
+      } else {
+        text = `L${++colorCounts.leftPurpleCount} P`;
+      }
+    } else {
+      if (ballColor === "red") {
+        text = `R${++colorCounts.rightRedCount} O`;
+      } else {
+        text = `R${++colorCounts.rightPurpleCount} P`;
+      }
     }
+    ball.setData("value", text);
+
+    const textObj = this.add.text(ballX, ballY, text.replace(' O', "").replace(' P', ""), { fontSize: "16px" });
+    textObj.setOrigin(0.5);
+    ball.setData("text", textObj);
 
     ball.on("pointerdown", () => {
-      this.stop();
+      if (this.isRunning) {
+        this.pause();
+        console.log(text);
+        this.config.onSelection({ win: this.selection === text, selected: text });
+      }
     });
 
     return ball;
   }
 
   start() {
+    this.physics.resume();
     for (const ball of this.orbyGroup.getChildren()) {
-      this.physics.moveToObject(ball, this.fyrakk, this.config.speed);
+      this.physics.moveToObject(ball, this.fyrakk, this.config.speed * (ball.getData('speed') || 1));
     }
+
+    // Create a countdown event that updates every second
+    // this.timedEvent = this.time.addEvent({
+    //   delay: 1000,
+    //   callback: this.onTimerTick,
+    //   callbackScope: this,
+    //   loop: true,
+    // });
   }
 
   stop() {
@@ -201,5 +261,16 @@ export class Intermission extends Phaser.Scene {
       this.orbyGroup.clear(true, true);
       this.orbies = [];
     }
+  }
+
+  pause() {
+    this.physics.pause();
+    this.isRunning = false;
+  }
+
+  // Function to format the time in MM:SS format
+  formatTime(milliseconds: number) {
+    const seconds = milliseconds / 1000;
+    return seconds.toFixed(2); // Format to two decimal places
   }
 }
